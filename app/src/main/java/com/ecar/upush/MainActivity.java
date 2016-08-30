7511bed2f4e00e956f526ce448bf11d031343c40
jinzheng519 <jinzheng519@163.com>
2016-08-30 15:06:06 +0800

    Initial commit

---
+++ app/src/main/java/com/ecar/upush/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ecar.upush;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.umeng.message.UmengMessageHandler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
