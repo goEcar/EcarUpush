c9c09a51232285cac610dee6c2e64d635ff4dbfc
jinzheng519 <jinzheng519@163.com>
2016-08-30 18:08:40 +0800

    update

--- app/src/main/java/com/ecar/upush/MainActivity.java
+++ app/src/main/java/com/ecar/upush/MainActivity.java
@@ -3,7 +3,6 @@ package com.ecar.upush;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.umeng.message.UmengMessageHandler;
 
 public class MainActivity extends AppCompatActivity {
 
